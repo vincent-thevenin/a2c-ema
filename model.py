a5e8fec8ad2673077876889f0d4b8312f1f06db3
Vincent <thevenin.vincent@hotmail.com>
2022-06-13 18:46:43 +0200

    Larger model

--- model.py
+++ model.py
@@ -23,12 +23,16 @@ class Q(nn.Module):
     def __init__(self, num_inputs, num_actions):
         super(Q, self).__init__()
 
-        self.lin1 = nn.Linear(num_inputs + num_actions, (num_inputs + num_actions) * 2)
-        self.lin2 = nn.Linear((num_inputs + num_actions) * 2, 1)
+        self.lin1 = nn.Linear(num_inputs + num_actions, 32)
+        self.lin2 = nn.Linear(32, 16)
+        self.lin3 = nn.Linear(16, 1)
 
     def forward(self, s, a):
         out = self.lin1(torch.cat((s,a), dim=-1))
-        out = torch.tanh(out)
+        out = torch.relu(out)
         out = self.lin2(out)
+        out = torch.relu(out)
+        out = self.lin3(out)
+
 
         return out
