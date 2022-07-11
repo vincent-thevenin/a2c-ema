f36fa5fea97563492eb30e8cb190ae7b99f29aa7
Vincent <thevenin.vincent@hotmail.com>
2022-07-11 18:56:32 +0200

    Change models to match sb

--- model.py
+++ model.py
@@ -6,33 +6,49 @@ class Actor(nn.Module):
     def __init__(self, num_actions, num_inputs):
         super(Actor, self).__init__()
         
-        self.lin1 = nn.Linear(num_inputs, 32)
-        self.lin2 = nn.Linear(32, 16)
-        self.lin3 = nn.Linear(16, num_actions)
+        self.lin1 = nn.Linear(num_inputs, 64)
+        self.lin2 = nn.Linear(64, 64)
+        self.lin3 = nn.Linear(64, num_actions)
     
     def forward(self, s):
         out = self.lin1(s)
-        out = torch.relu(out)
+        out = torch.tanh(out)
         out = self.lin2(out)
-        out = torch.relu(out)
+        out = torch.tanh(out)
         out = self.lin3(out)
         
         return out
-    
+
 class Q(nn.Module):
     def __init__(self, num_inputs, num_actions):
         super(Q, self).__init__()
 
-        self.lin1 = nn.Linear(num_inputs + num_actions, 32)
-        self.lin2 = nn.Linear(32, 16)
-        self.lin3 = nn.Linear(16, 1)
+        self.lin1 = nn.Linear(num_inputs + num_actions, 64)
+        self.lin2 = nn.Linear(64, 64)
+        self.lin3 = nn.Linear(64, 1)
 
     def forward(self, s, a):
-        out = self.lin1(torch.cat((s,a), dim=-1))
-        out = torch.relu(out)
+        out = self.lin1(torch.cat((s, a), dim=-1))
+        out = torch.tanh(out)
         out = self.lin2(out)
-        out = torch.relu(out)
+        out = torch.tanh(out)
         out = self.lin3(out)
 
+        return out
+
+class V(nn.Module):
+    def __init__(self, num_inputs):
+        super(V, self).__init__()
+
+        self.lin1 = nn.Linear(num_inputs, 64)
+        self.lin2 = nn.Linear(64, 64)
+        self.lin3 = nn.Linear(64, 1)
+
+    def forward(self, s):
+        out = self.lin1(s)
+        out = torch.tanh(out)
+        out = self.lin2(out)
+        out = torch.tanh(out)
+        out = self.lin3(out)
 
         return out
